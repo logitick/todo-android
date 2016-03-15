24014aba905196a301fa9200e0f4e0399bcb8758
Paul Daniel Iway <paul.daniel.iway@gmail.com>
2016-03-15 08:18:48 +0800

    list up but trying to have the toolbar show up on addtodoactivity

---
+++ README.md
@@ -0,0 +1,7 @@
+# A demo Todo app for learning android development
+
+## ActionBars / Toolbars
+* Cannot just add a base activity for all activities to have an action bar see [http://developer.android.com/training/appbar/index.html](http://developer.android.com/training/appbar/index.html);
+
+## RecyclerView
+* Easy-peasey but it looks meh reference [http://code.tutsplus.com/tutorials/getting-started-with-recyclerview-and-cardview-on-android--cms-23465](http://code.tutsplus.com/tutorials/getting-started-with-recyclerview-and-cardview-on-android--cms-23465)
--- app/src/main/java/io/github/logitick/todo/TodoListDataAdapter.java
+++ app/src/main/java/io/github/logitick/todo/TodoListDataAdapter.java
@@ -1,6 +1,8 @@
 package io.github.logitick.todo;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -14,17 +16,26 @@ public class TodoListDataAdapter extends RecyclerView.Adapter<TodoListDataAdapte
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_layout, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        holder.setHeading(dataset[position]);
+        holder.setDescription(dataset[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if (dataset == null) {
+            return 0;
+        }
+        return dataset.length;
     }
 
     // Provide a reference to the views for each data item
@@ -32,10 +43,21 @@ public class TodoListDataAdapter extends RecyclerView.Adapter<TodoListDataAdapte
     // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView mTextView;
-        public ViewHolder(TextView v) {
+        private TextView headingTextView;
+        private TextView descriptionTextView;
+        public ViewHolder(View v) {
             super(v);
-            mTextView = v;
+            headingTextView = (TextView) v.findViewById(R.id.todo_list_item_heading);
+            descriptionTextView = (TextView) v.findViewById(R.id.todo_list_item_description);
         }
+
+        public void setHeading(String str) {
+            this.headingTextView.setText(str);
+        }
+
+        public void setDescription(String str) {
+            this.descriptionTextView.setText(str);
+        }
+
     }
 }
